9235bc87ac652b1fbb7eda5e4b83fe3e62508901
SoloDactyl <flayerhost@gmail.com>
2025-01-04 02:15:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Welcome to the SoloDactyl Panel Installer!"
+echo "Welcome to the SoloDactyl Docker Installer!"
 
 echo "Please select an option:"
-echo "1 - Install SoloDactyl and Docker"
-echo "2 - Uninstall SoloDactyl and Docker"
+echo "1 - Install SoloDactyl Wings "
+echo "2 - Uninstall SoloDactyl Wings "
 read -p "Enter your choice: " choice
 
 if [[ $choice -eq 1 ]]; then
 
-    echo "Setting up SoloDactyl Panel..."
+    echo "Setting up SoloDactyl Wings..."
     # Install Node.js and dependencies
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
@@ -18,33 +18,24 @@ if [[ $choice -eq 1 ]]; then
     sudo apt install -y nodejs git
 
     # Clone SoloDactyl repository
-    git clone https://github.com/Solodactyl/solodactyl-panel.git
-    cd solodactyl-panel
+    git clone https://github.com/Solodactyl/solodactyl-wings.git
+    cd solodactyl-wings
 
-    # Install dependencies and set up the panel
+    # Install dependencies and set up the Wings
     npm install
-    npm run seed
-    npm run createUser
 
-    # Run the panel
-    node .
-
-    echo "Installation completed! SoloDactyl Panel is running."
+    echo "Wings Installation completed!"
 
 
 
 elif [[ $choice -eq 2 ]]; then
-    echo "Uninstalling SoloDactyl Panel and Docker..."
-    # Remove SoloDactyl panel files
-    sudo rm -rf solodactyl-panel
-    sudo rm -rf solodactyl-wings
 
     # Uninstall Docker
     sudo apt-get remove -y docker.io
     sudo rm -rf /var/lib/docker
     sudo rm -rf /etc/docker
 
-    echo "Uninstallation completed."
+    echo "Wings Uninstallation completed."
 else
     echo "Invalid choice. Exiting."
 fi
